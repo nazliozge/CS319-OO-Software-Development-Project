77d43e925b148e625688df8fc8e67ec8cc62f3da
medermand <Meder@MacBook-Air-5.local>
2016-05-03 10:00:20 +0300

    last version

--- Implementation/RiverAdventures/src/UserInterface/Menus/PauseMenu.java
+++ Implementation/RiverAdventures/src/UserInterface/Menus/PauseMenu.java
@@ -29,32 +29,14 @@ public class PauseMenu extends Menu{
 		JButton resumeButton=new JButton("RESUME");
 		JButton mainButton=new JButton("EXIT");
 		JButton settingsButton=new JButton("SETTINGS");
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-		//JButton highscoreButton=new JButton("HIGHSCORES");
-		JButton helpButton=new JButton("HELP");
->>>>>>> origin/master
 
 		Color newColor = new Color(74, 192, 195);
 		setBackground(newColor);
 
 
 
-<<<<<<< Updated upstream
-		//highscoreButton.setSize(390,50);
-		//highscoreButton.setLocation(140,240);
-=======
-<<<<<<< HEAD
 		resumeButton.setSize(XSize,YSize);
 		resumeButton.setLocation(XLocation,130);
-=======
-		//highscoreButton.setSize(390,50);
-		//highscoreButton.setLocation(140,240);
->>>>>>> origin/master
->>>>>>> Stashed changes
 
 		settingsButton.setSize(XSize,YSize);
 		settingsButton.setLocation(XLocation,180);
@@ -75,21 +57,6 @@ public class PauseMenu extends Menu{
 				settingScreen();
 			}
 		});
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-		//highscoreButton.addActionListener(new ActionListener(){
-		//	public void actionPerformed(ActionEvent e){
-			//	highscoreScreen();
-			//}
-		//});
-		helpButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				helpScreen();
-			}
->>>>>>> origin/master
 
 
 		mainButton.addActionListener(new ActionListener(){
@@ -101,14 +68,6 @@ public class PauseMenu extends Menu{
 
 		add(resumeButton);
 		add(settingsButton);
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-		//add(highscoreButton);
-		add(helpButton);
->>>>>>> origin/master
 		add(mainButton);
 
 	};
@@ -133,19 +92,7 @@ public class PauseMenu extends Menu{
 	public void resumeGame(){
 		mng.requestUnpause();
 	}
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 
-=======
->>>>>>> Stashed changes
-	//public void highscoreScreen(){
-		//mng.requestHighscores();
-	//}
-	public void helpScreen(){
-		mng.requestHelp();
-	}
->>>>>>> origin/master
 	public void mainScreen(){
 		mng.requestMain();
 	}
